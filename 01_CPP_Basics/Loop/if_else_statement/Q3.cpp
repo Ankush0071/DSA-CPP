392076b4f6094892e4959bc2ab83c0feebe47f1f
Ankush0071 <Ankushgupta0071@gmail.com>
2026-08-03 23:50:34 +0530

    Initial DSA upload

---
+++ 01_CPP_Basics/Loop/if_else_statement/Q3.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+Take the age from the user and then decide accordingly
+1. if age < 18,
+print -> not eligible for job 
+2. if age =18,
+print -> "eligible for job"
+3. if age <= 55 and age <= 57,
+print -> "eligible for job, but retirement soon."
+4. if age > 57
+print -> "retirement time"
+*/
+int main (){
+    
+    return 0;
+}
